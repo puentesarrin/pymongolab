9373ac0fc64f045bb38cb57579e97f3a9c3da05f
Jorge Puente Sarrín <puentesarrin@gmail.com>
2013-05-28 11:48:17 -0500

    Adding version methods

--- pymongolab/__init__.py
+++ pymongolab/__init__.py
@@ -19,5 +19,16 @@ ALL = 2
 """Profile all operations."""
 
 
+version_tuple = (1, 2, '+')
+
+
+def get_version_string():
+    if isinstance(version_tuple[-1], basestring):
+        return '.'.join(map(str, version_tuple[:-1])) + version_tuple[-1]
+    return '.'.join(map(str, version_tuple))
+
+version = get_version_string()
+
+
 from pymongolab.connection import Connection
 from pymongolab.mongo_client import MongoClient
